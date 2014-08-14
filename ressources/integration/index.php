ee3e8e3a1271dbdcf7e256be32027b25c277d891
Cloé <cloe.duc@gmail.com>
2014-08-14 19:41:20 +0200

    transform tables to section on homepage

--- ressources/integration/index.php
+++ ressources/integration/index.php
@@ -9,108 +9,80 @@ include('parts/header.php'); ?>
         <li><img src="img/icons/globe.png"/>La campagne</li>
         <li class="active">Le forum</li>
     </ol>
-    <div class="row">
-    <div class="col-md-10">
-     <div class="alert alert-success " role="alert">
-         Poney
-         <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-    </div>
-        </div>
-        </div>
-    <h1>
-        Le forum
-    </h1>
-
 </section>
 
 <!-- Main content -->
 <section class="content">
-<table class="table table-responsive categorie-section">
-      <thead>
-        <tr>
-          <th class="cat-title">                        
+     <div class="alert alert-success " role="alert">
+         Poney
+         <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+    </div>
+    <h1>
+        Le forum
+    </h1>
+    <?php for($e=1; $e <= 4; $e++) :?>
+    <section class="categorie-section">
+        <header class="cat-title">
             <div class="pull-left">
                 <img src="img/icons/magicwand-bgless.png"/>
             </div><h1>Ministère de la magie</h1>
-            </th>
-            <th class="last-message cat-title"><span class="glyphicon glyphicon-star"></span></th>
-        </tr>
-      </thead>
-      <tbody>
+
+        </header>
       <?php for($i=1; $i <= 5; $i++) :?>
-            <tr class="start-entry <?php if($i%3 == 0):?>new-message<?php endif; ?>">
-              <td class="cat-description">
+        <article class="start-entry <?php if($i%3 == 0):?>new-message<?php endif; ?>">
+            <div class="cat-description">
                 <h2><a href="forum.php">La salle d'entrainement</a></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. </p>
-              </td>
-              <td class="last-messages"><a <?php if($i%3 == 0):?>data-toggle="tooltip" data-placement="top" title="3 sujets non lus" <?php endif; ?> href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></td>
-            </tr>
+            </div>
+            <aside class="last-messages">
+                <a class="display-last-messages" <?php if($i%3 == 0):?>data-toggle="tooltip" data-placement="top" title="3 sujets non lus" <?php endif; ?> href="#"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                 <?php for($j=1; $j <= 3; $j++) :?>
+                <article class="message-list">
+                    <a href="subject.php#p26">
+                    <h2>Réparation en urgence</h2>
+                    <p class="last-message-info">Dernier message par Esther Mycroft le 26 juin 1989</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. </p>
+                    </a>
+                </article>
+                 <?php endfor; ?>
+            </aside>
+        </article>
     <?php endfor; ?>
-      </tbody>
-    </table>
-<table class="table table-responsive categorie-section">
-      <thead>
-        <tr>
-          <th colspan="4" class="cat-title">                        
+    </section>
+    <?php endfor; ?>
+    <section class="categorie-section">
+        <header class="cat-title">
             <div class="pull-left">
-                <img src="img/icons/magicwand-bgless.png"/>
-            </div><h1>Près au Lard</h1>
-            </th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php for($i=1; $i <= 3; $i++) :?>
-            <tr class="start-entry">
-              <td rowspan="2" class="cat-icon"></td>
-              <td rowspan="2" class="cat-description"><h2><a href="#">La salle d'entrainement</a></h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. </p></td>
-              <th class="to-read">A lire (4)</th>
-              <th class="last-message">Dernier message</th>
-            </tr>
-            <tr>
-              <td class="to-read">
-                <a href="#">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    <span class="glyphicon glyphicon-share-alt  btn-xs"></span>
-                </a>
-                </td>
-              <td class="last-message"><a href="#">Esther Cromwell</a><br /> hier à 13h08</td>
-            </tr>
+                                <img src="img/icons/radio.png"/>
+            </div>
+            <h1>Discuter de la campagne</h1>
+
+        </header>
+      <?php for($i=1; $i <= 5; $i++) :?>
+        <article class="start-entry <?php if($i%3 == 0):?>new-message<?php endif; ?>">
+            <div class="cat-description">
+                <h2><a href="forum.php">La salle d'entrainement</a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. </p>
+            </div>
+            <aside class="last-messages">
+                <a class="display-last-messages" <?php if($i%3 == 0):?>data-toggle="tooltip" data-placement="top" title="3 sujets non lus" <?php endif; ?> href="#"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                 <?php for($j=1; $j <= 3; $j++) :?>
+                <article class="message-list">
+                    <a href="subject.php#p26">
+                    <h2>Réparation en urgence</h2>
+                    <p class="last-message-info">Dernier message par Esther Mycroft le 26 juin 1989</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. </p>
+                    </a>
+                </article>
+                 <?php endfor; ?>
+            </aside>
+        </article>
     <?php endfor; ?>
-      </tbody>
-    </table>
-<table class="table table-responsive categorie-section">
-      <thead>
-        <tr>
-          <th colspan="4" class="cat-title">                        
-            <div class="pull-left">
-                <img src="img/icons/radio.png"/>
-            </div><h1>Discuter de la campagne</h1>
-            </th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php for($i=1; $i <= 2; $i++) :?>
-            <tr class="start-entry">
-              <td rowspan="2" class="cat-icon"></td>
-              <td rowspan="2" class="cat-description"><h2><a href="#">La salle d'entrainement</a></h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. </p></td>
-              <th class="to-read">A lire (4)</th>
-              <th class="last-message">Dernier message</th>
-            </tr>
-            <tr>
-              <td class="to-read">
-                <a href="#">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    <span class="glyphicon glyphicon-share-alt  btn-xs"></span>
-                </a>
-                </td>
-              <td class="last-message"><a href="#">Esther Cromwell</a><br /> hier à 13h08</td>
-            </tr>
-    <?php endfor; ?>
-      </tbody>
-    </table>
+    </section>
     </div>
 
 </section><!-- /.content -->
 
 <?php include('parts/userbar.html'); ?>
-<?php $currenttpl = 'index'; include('parts/footer.php'); ?>
+<?php $currenttpl = 'index'; 
+include('parts/footer.php'); ?>
