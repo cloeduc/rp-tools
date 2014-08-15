15396cef21b3bcefc82f1f180c1e71b31a190900
Cloé <cloe.duc@gmail.com>
2014-08-16 00:16:02 +0200

    remove admin js

--- ressources/integration/parts/footer.php
+++ ressources/integration/parts/footer.php
@@ -7,8 +7,6 @@
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/role-play-tools.js" type="text/javascript"></script>
 
-        <!-- AdminLTE App -->
-        <!--<script src="js/AdminLTE/app.js" type="text/javascript"></script>-->
         <!-- bootbox code -->
         <script src="js/plugins/bootbox/bootbox.min.js"></script>
         <script>
